408c536335745f4c9e160c8b08ce3974cbdc006e
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-06-07 07:51:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.use(function (req, res) {
 })
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`Riptide is running at localhost:${port}`)
+  console.log(`Ultrabrowse is running at localhost:${port}`)
 });
